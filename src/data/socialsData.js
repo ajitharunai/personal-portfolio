a1ea1864bea0a633548453a56d02e525c8f89568
AJITH KUMAR M <89124776+ajitharunai@users.noreply.github.com>
2023-01-29 08:07:58 +0530

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -7,7 +7,7 @@ export const socialsData = {
     twitter: 'https://twitter.com/ajitharunai',
     reddit: 'https://www.reddit.com/user/',
     blogger: 'ajicreations.blogspot.com',
-    medium: 'https://medium.com/@ajithkumaraec10',
+    medium: 'https://medium.com/@ajitharunai',
     stackOverflow: 'https://stackoverflow.com/users/19111122/ajith-kumar-m',
     gitlab: 'https://gitlab.com/ajitharunai',
     youtube: 'https://www.youtube.com/channel/UCziq6bcqqPG9EzIM_H3gMUA'
