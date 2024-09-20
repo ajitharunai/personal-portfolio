dc0b1af9c19cfdc8e55f89d830da048736cf93ca
AJITH KUMAR M <89124776+ajitharunai@users.noreply.github.com>
2024-09-20 15:25:06 +0530

    data updateds

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,55 +1,54 @@
 /* eslint-disable */
-import girl1 from '../assets/svg/testimonials/girl1.png'
+import girl1 from '../assets/svg/testimonials/girl1.svg'
 import girl2 from '../assets/svg/testimonials/girl2.svg'
-import girl3 from '../assets/svg/testimonials/girl3.png'
-import boy1 from '../assets/svg/testimonials/boy1.png'
+import girl3 from '../assets/svg/testimonials/girl3.svg'
+import boy1 from '../assets/svg/testimonials/boy1.svg'
 import boy2 from '../assets/svg/testimonials/boy2.svg'
-import boy3 from '../assets/svg/testimonials/boy3.png'
+import boy3 from '../assets/svg/testimonials/boy3.svg'
 
 
 
 export const testimonialsData = [
     {
         id: 1,
-        name: 'Albert Einstein',
-        title: 'Scientist',
-        text: "I have no special talent. I am only passionately curious.",
+        name: 'Chandler Bing',
+        title: 'Transponster',
+        text: "Building my own portfolio could've never been easier than this. The only areas I'm good at are Math, Jokes and Dance.",
         image: boy2
     },
     {
         id: 2,
-        name: 'Thomas Edison',
-        title: 'American Inventor',
-        text: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+        name: 'Monica Geller',
+        title: 'Head Chef',
+        text: "This is the only thing that I didn't have to spend time to sort out. It's tidy all by itself.",
         image: girl2
     },
     {
         id: 3,
-        name: 'Sundar Pichai',
-        title: 'CEO,Google',
-        text: "As a leader, It is important to not just see your own success, but focus on the success of others.",
+        name: 'Phoebe Buffay',
+        title: 'Massuese',
+        text: "Heard of the song 'Smelly Cat!!' ? This website made it viral. Plus portfolio is the only thing that Ursula doesn't have :)",
         image: girl1
     },
     {
         id: 4,
-        name: 'satya nadella',
-        title: 'CEO,Microsoft',
-        text: "We needed to build deeper empathy for our customers and their unarticulated and unmet needs. It was time to hit refresh.",
+        name: 'Joey Tribbiani',
+        title: 'Actor',
+        text: "Making my portfolio with this helped me get my role as 'Dr. Drake Ramoray' in 'Days Of Our Lives'. You guys are the best !",
         image: boy1
     },
     {
         id: 5,
-        name: 'Elon Musk ',
-        title: 'CEO,Tesla',
-        text: "If you get up in the morning and think the future is going to be better, it is a bright day. Otherwise, it's not.",
+        name: 'Rachel Green',
+        title: 'Sales Executive',
+        text: 'Ralph Lauren was one of the best things that ever happened to me. My portfolio made that dream happen. I owe you guys.',
         image: girl3
     },
     {
         id: 6,
-        name: 'APJ Abdul Kalam',
-        title: 'Scientist',
-        text: "Creativity is the key to success in the future, and primary education is where teachers can bring creativity in children at that level.",
+        name: 'Ross Geller',
+        title: 'Paleontologist',
+        text: "Be it 'Rocks' or 'Bones', you need a website to display it. This is it. Great work guys !",
         image: boy3
     }
 ]
-
